ba9bb0e8d1a298f22db6e53cfa2027246ed89a0d
Carlitos000 <>
2023-05-15 08:58:07 +0000

    Set the board structure with 3 rows

--- run.py
+++ run.py
@@ -4,14 +4,18 @@
 import random
 
 boardGame = ["-", "-", "-",
-        "-", "-", "-",
-        "-", "-", "-"]
+             "-", "-", "-",
+            "-", "-", "-"]
 
 game = True
 winner = None
 player = "X"
 
 # game structure
+def printBoard(board):
+    print(board[0] + " | " + board[1] + " | " + board[2])
+    print(board[3] + " | " + board[4] + " | " + board[5])
+    print(board[6] + " | " + board[7] + " | " + board[8])
 
 # player input
 
